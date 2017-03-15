5d901c0a8b9b2f6388d7a5402dc473d0efad11bf
Greg Poisson <gpoisson@rams.colostate.edu>
2017-03-15 09:59:15 -0600

    testing

--- src/cs455/scaling/Node.java
+++ src/cs455/scaling/Node.java
@@ -3,6 +3,6 @@ package cs455.scaling;
 // Interface for server and client nodes. Mainly used to turn debug mode on/off
 public interface Node {
 
-	public boolean debug = false;
+	public boolean debug = true;
 	
 }
